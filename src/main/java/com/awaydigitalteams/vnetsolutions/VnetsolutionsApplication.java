29c488ab052c0c1ea48ab003f8f967bd7805f188
Hung Pham <tienhung278@gmail.com>
2023-05-29 06:51:54 +0800

    Init commit

---
+++ src/main/java/com/awaydigitalteams/vnetsolutions/VnetsolutionsApplication.java
@@ -0,0 +1,16 @@
+package com.awaydigitalteams.vnetsolutions;
+
+import com.awaydigitalteams.vnetsolutions.service.Scheduler;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class VnetsolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VnetsolutionsApplication.class, args);
+		var scheduler = new Scheduler();
+	}
+}
---
+++ src/test/java/com/awaydigitalteams/vnetsolutions/VnetsolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awaydigitalteams.vnetsolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VnetsolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
