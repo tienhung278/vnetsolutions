d8dd4c17fe6da2dd66d04a6df3d5d8dcc9f303bc
Hung Pham <tienhung278@gmail.com>
2023-05-30 11:49:40 +0800

    Implement CORS

--- src/main/java/com/awaydigitalteams/vnetsolutions/api/SaleController.java
+++ src/main/java/com/awaydigitalteams/vnetsolutions/api/SaleController.java
@@ -3,6 +3,7 @@ package com.awaydigitalteams.vnetsolutions.api;
 import com.awaydigitalteams.vnetsolutions.dto.SaleProductDTO;
 import com.awaydigitalteams.vnetsolutions.service.SaleServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,7 @@ public class SaleController {
         this.saleServices = saleServices;
     }
 
+    @CrossOrigin
     @GetMapping("/unitsrevenuebyproduct")
     public List<SaleProductDTO> getUnitsRevenueByProduct() {
         return saleServices.getUnitsRevenueProduct();
